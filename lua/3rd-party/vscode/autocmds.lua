d15423b790b35c96878a3a0dc166f84a958176f8
N07Cc <N07Cc@foxmail.com>
2026-03-04 17:28:25 +0800

    update(vscode): remove auto commands for automatic IME switching

--- lua/3rd-party/vscode/autocmds.lua
+++ lua/3rd-party/vscode/autocmds.lua
@@ -7,22 +7,22 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 })
 
 -- 自动切换输入法
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-    pattern = "*",
-    callback = function()
-        local input_status = tonumber(vim.fn.system("D:\\Programs\\im-select\\im-select.exe"))
-        if input_status == 2052 then
-            vim.fn.system("D:\\Programs\\im-select\\im-select.exe 1033")
-            input_status = tonumber(vim.fn.system("D:\\Programs\\im-select\\im-select.exe"))
-        end
-    end,
-})
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-    pattern = "*",
-    callback = function()
-        local input_status = tonumber(vim.fn.system("D:\\Programs\\im-select\\im-select.exe"))
-        if input_status == 1033 then
-            vim.fn.system("D:\\Programs\\im-select\\im-select.exe 2052")
-        end
-    end,
-})
+-- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+--     pattern = "*",
+--     callback = function()
+--         local input_status = tonumber(vim.fn.system("D:\\Programs\\im-select\\im-select.exe"))
+--         if input_status == 2052 then
+--             vim.fn.system("D:\\Programs\\im-select\\im-select.exe 1033")
+--             input_status = tonumber(vim.fn.system("D:\\Programs\\im-select\\im-select.exe"))
+--         end
+--     end,
+-- })
+-- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+--     pattern = "*",
+--     callback = function()
+--         local input_status = tonumber(vim.fn.system("D:\\Programs\\im-select\\im-select.exe"))
+--         if input_status == 1033 then
+--             vim.fn.system("D:\\Programs\\im-select\\im-select.exe 2052")
+--         end
+--     end,
+-- })
